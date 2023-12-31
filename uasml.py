01c80d86ee0c862660d1ae6f308e7b9aa085e96c
Kresnaprayoga17 <49635294+Kresnaprayoga17@users.noreply.github.com>
2023-12-31 11:31:05 +0700

    Update uasml.py

--- uasml.py
+++ uasml.py
@@ -3,20 +3,20 @@ import pandas as pd
 import numpy as np
 from mlxtend.frequent_patterns import association_rules, apriori
 
-bakery = pd.read_csv('BreadBasket_DMS.csv')
-bakery['Datetime'] = pd.to_datetime(bakery['Date'] + ' ' + bakery['Time'], format='%Y-%m-%d %H:%M:%S')
-bakery['Datetime'] = pd.to_datetime(bakery['Datetime'], format= "%d-%m-%Y")
+df = pd.read_csv('BreadBasket_DMS.csv')
+df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'], format='%Y-%m-%d %H:%M:%S')
+df['Datetime'] = pd.to_datetime(df['Datetime'], format= "%d-%m-%Y")
 
-bakery["month"] = bakery['Datetime'].dt.month
-bakery["day"] = bakery['Datetime'].dt.day
+df["month"] = df['Datetime'].dt.month
+df["day"] = df['Datetime'].dt.weekday
 
-bakery["month"].replace([i for i in range(1, 12 + 1)], ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustur","September","Oktober","November","Desember"], inplace=True)
-bakery["day"].replace([i for i in range(6 + 1)], ["senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"],inplace=True)
+df["month"].replace([i for i in range(1, 12 + 1)], ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustur","September","Oktober","November","Desember"], inplace=True)
+df["day"].replace([i for i in range(6 + 1)], ["senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"],inplace=True)
 
 st.title("UAS Transaction from a bakery Algoritma Apriori")
 
 def get_bakery( month ='' , day = ''):
-    bakery = bakery.copy()
+    bakery = df.copy()
     filtered = bakery.loc[
         (bakery["month"].str.contains(month.title())) &
         (bakery["day"].str.contains(day.title()))
